99655edc67fd4cc18d4885f3cb2ed832fd7140e3
OMPedr0_ <59456575+OMPedr0@users.noreply.github.com>
2023-03-18 10:02:39 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,11 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
+
 //
 // Scripts
-// 
+//
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -52,3 +54,55 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const zksyncButton = document.querySelector('.connect-zksync');
+const metamaskButton = document.getElementById('metamask-button');
+let web3;
+let zksync
+let accounts;
+
+
+let accountElem = document.querySelector('#account-address');
+
+metamaskButton.addEventListener('click', async () => {
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed!');
+    await connectMetamask();
+  } else {
+    console.log('MetaMask not found');
+  }
+});
+
+zksyncButton.addEventListener('click', async () => {
+  try {
+    if (typeof window.zksync !== 'undefined') {
+      console.log('zkSync SDK is installed!');
+      await connectZksync();
+    } else {
+      console.log('zkSync SDK not found');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+async function connectMetamask() {
+  accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  account = accounts[0];
+  console.log('Your address:', account);
+  accountElem.innerHTML = account;
+  metamaskButton.textContent = 'Is logged';
+}
+
+async function connectZksync() {
+  // Use o SDK JavaScript do zkSync para conectar-se à carteira do usuário.
+  const provider = await zksync.getDefaultProvider('0xb50388c68cef4fadc150674dfb5746d8');
+  const syncWallet = await zksync.Wallet.fromEthSigner(window.ethereum, provider);
+  console.log('zkSync is connected');
+  accounts = await syncWallet.getEthereumAccounts();
+  account = accounts[0].address;
+  console.log('Your address:', account);
+  accountElem.innerHTML = account;
+  zksyncButton.textContent = 'Is logged';
+}
+
